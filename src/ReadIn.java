7c6d12df8045eecdbc2b817ef977ed807edc2c43
Caden-Mac <107044782+Caden-Mac@users.noreply.github.com>
2022-06-07 08:45:17 -0400

    New ReadIn

--- src/ReadIn.java
+++ src/ReadIn.java
@@ -1,3 +1,5 @@
+//TEST
+
 //Cadence McCarthy Final Project
 //Reads in assignment file
       //Fields, constructors, getters, setters, toString
@@ -21,13 +23,14 @@ public class ReadIn {
 
    public ReadIn(String filename) {
    // SET THE FILENAME field value here
+
+   this.filename = filename;
+   int count = countLines(); 
    
-   
-   // CALL COUNT LINES HERE ... so you have count to create the array!
-   // STORE THE COUNT (it can be a local variable)
+            // CALL COUNT LINES HERE ... so you have count to create the array!
+            // STORE THE COUNT (it can be a local variable)
      
      //Array
-     int count = countLines();
       assign_Info = new Assignment[count];
 
    //Read & Write the file
@@ -38,7 +41,13 @@ public class ReadIn {
 
    private Scanner openFile() {
       File file = new File(filename);
-      Scanner scanner = new Scanner(file);
+      Scanner scanner = null;
+      try {
+         scanner = new Scanner(file);
+      } catch(FileNotFoundException e) {
+        System.out.println("Did not find file: " + filename);
+        System.exit(-1);
+      }
    
       return scanner;
    }
@@ -56,7 +65,8 @@ public class ReadIn {
       file.close();
       return count;
    }
-   public void setFilename(String filename) {
+   
+      public void setFilename(String filename) {
       this.filename = DEFAULT_FILE;
    }
 
@@ -67,9 +77,10 @@ public class ReadIn {
    LocalDate dueDate = null;
    int classNum = 0;
    String notes = null;
+   boolean complete = false;
    
    //Open the file
-      Scanner file = openFile(filename);
+      Scanner file = openFile();
       int count = 0;
      
 
@@ -84,29 +95,44 @@ public class ReadIn {
          // put the parts into the local variables (will require some conversions - date/int)
          
          name = parts[0];
-         dueDate = parts[1];
-         classNum = parts[2];
+         dueDate = LocalDate.ofEpochDay(Long.parseLong(parts[1]));       //LocalDate date2 = LocalDate.ofEpochDay(numDate);
+         classNum = Integer.parseInt(parts[2]);
          notes = parts[3];
+         complete = Boolean.parseBoolean(parts[4]);
          
          // now that you have all the parts, make an assignment and put it into the assign_Info array.
          
-         Assignment assignment = // create the assignment here
-         array[arrayIndex] = yourAssignment; // put assignment into array here
-                                 // increment the arrayIndex
-         System.out.println(line);
-      }
+         Assignment assignment = new Assignment(name, dueDate, classNum, notes, complete);// create the assignment here
+         assign_Info[assignNo] = assignment; // put assignment into array here
+         assignNo++;                        // increment the arrayIndex
+  //        System.out.println(line);
+       }
          //Close the file
       file.close();
 
    }
+   
+         public void printAll() {
+         for (int i = 0; i < assign_Info.length; i++) {
+            System.out.println(assign_Info[i]);
+         }
+         System.out.println("Done");
+      }
+      
+      
 }
 
 class Tester { 
    
    public static void main(String[] args) {
-   
-      new ReadIn();
+      
+      
+      ReadIn test = new ReadIn();
+      
+      test.printAll();
       //new ReadIn("this is my actual file.csv");
+      
+
    
    }
 
