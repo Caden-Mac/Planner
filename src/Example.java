fdb9938d2308c301001a3be6c293ddaf5ac277b0
Terri Kelly <kellyt@bishopireton.org>
2022-06-10 16:01:05 -0400

    Fixed Example three

--- src/Example.java
+++ src/Example.java
@@ -8,15 +8,17 @@ import javax.swing.JTextField;
 import java.awt.Font;
 import javax.swing.JTextArea;
 import java.awt.Component;
+import java.awt.FlowLayout;
+import javax.swing.JCheckBox;
 
 public class Example extends JPanel {
 
 
 // Only do one example at a time.  THe first one should be sufficient
    public Example() {
-      exampleOne();
- //     exampleTwo();
-//     exampleThree();   
+   //    exampleOne();
+   //     exampleTwo();
+      exampleThree();   
    }
    
    private void exampleOne() {
@@ -38,9 +40,7 @@ public class Example extends JPanel {
       
       // WITHOUT THIS LINE - all of the JLABELS appear in the center of the panel,
       // WITH THIS LINE, everyhting is on the left ... try commenting it out and running
-      bigText.setAlignmentX(Component.LEFT_ALIGNMENT);
-      
-      
+      bigText.setAlignmentX(Component.LEFT_ALIGNMENT);      
       bigText.setText("I am the very model of a modern major General\nI've information vegetable, animal, and mineral\nI know the kings of England, and I quote the fights Historical\nFrom Marathon to Waterloo, in order categorical");
       add(bigText);
       add(Box.createRigidArea(new Dimension(20, 50)));  // bigger space at bottom 
@@ -78,25 +78,53 @@ public class Example extends JPanel {
    }
 
 
-// This is not done - it was going to show you how to put the 
-// label on the left and the textfield on the right
+// This puts the label for the field on the same line as the field
+// and makes the textfield actually be only as big as we want it
 
    private void exampleThree() {
-     setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-     setAlignmentX(Component.LEFT_ALIGNMENT);
-    add( pairs("One", 10, Color.BLUE));
-    add( pairs("Two", 20, Color.RED));
+      setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+      add( pairs("One", 10, Color.BLUE));
+      add( pairs("Two", 30, Color.RED));
+      add( pairs("Three", 20, Color.GREEN));
+      add( pairsCheckBox("Completed", Color.YELLOW));
+      add( pairsArea("Four", Color.ORANGE));
+      setAlignmentX(Component.LEFT_ALIGNMENT);
    }
-private JPanel pairs(String labelName, int textfieldSize, Color color) {
-  JPanel panel = new JPanel();
-  panel.setBackground(color);
-  JLabel one = CommonGUI.createLabel(labelName);
-  one.setAlignmentX(Component.LEFT_ALIGNMENT);
-  panel.add(CommonGUI.createLabel(labelName));
-  panel.add(CommonGUI.createField(textfieldSize));
-  panel.setAlignmentX(Component.LEFT_ALIGNMENT);
-  return panel;
-}
+   
+   private JPanel pairs(String labelName, int textfieldSize, Color color) {
+      FlowLayout layout = new FlowLayout(FlowLayout.LEFT);
+      layout.setHgap(25);
+      JPanel panel = new JPanel(layout);
+      panel.setBackground(color);
+      panel.add(CommonGUI.createLabel(labelName));
+      panel.add(CommonGUI.createField(textfieldSize));
+      return panel;
+   }
+
+   private JPanel pairsCheckBox(String labelName, Color color) {
+      FlowLayout layout = new FlowLayout(FlowLayout.LEFT);
+      layout.setHgap(25);
+      JPanel panel = new JPanel(layout);
+      panel.setBackground(color);
+      panel.add(new JCheckBox(labelName));
+      return panel;
+   }
+
+   private JPanel pairsArea(String labelName, Color color) {
+      FlowLayout layout = new FlowLayout(FlowLayout.LEFT);
+      layout.setHgap(25);
+      JPanel panel = new JPanel(layout);
+   
+      panel.setBackground(color);
+   
+      panel.add(CommonGUI.createLabel(labelName));
+   
+      JTextArea bigText = new JTextArea();      
+      bigText.setText("I am the very model of a modern major General\nI've information vegetable, animal, and mineral\nI know the kings of England, and I quote the fights Historical\nFrom Marathon to Waterloo, in order categorical");
+   
+      panel.add(bigText);
+      return panel;
+   }
 
 }
 
